0a3c6f3843ce1ed51f8672560d0dddd963a7f1e8
Hitesh Sundra <hiteshsundra7@gmail.com>
2026-05-19 22:49:35 +0800

    SemZero column remove used smoke mutation

--- models/intermediate/int_payment_summary.sql
+++ models/intermediate/int_payment_summary.sql
@@ -2,12 +2,7 @@ WITH payment_summary AS (
     SELECT
         customer_id,
         COUNT(payment_id) AS total_payments,
-        SUM(payment_value) AS total_paid,
-        CASE 
-            WHEN SUM(CASE WHEN payment_status = 'completed' THEN 1 ELSE 0 END) > 0 
-            THEN 'paid'
-            ELSE 'pending'
-        END AS final_payment_status
+        SUM(payment_value) AS total_paid
     FROM {{ ref('stg_payments') }}
     GROUP BY customer_id
 )
